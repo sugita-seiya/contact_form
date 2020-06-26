204057ea0366f8b3b8e3f533f57c2a1cf0422a9e
sugita-seiya <satreu16@ezweb.ne.jp>
2020-06-27 02:09:34 +0900

    マークアップの実装

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,13 @@
 <?php
+// 入力ページ
+Route::get('/contact', 'ContactController@index')->name('contact.index');
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+//確認ページ
+Route::post('/contact/confirm', 'ContactController@confirm')->name('contact.confirm');
+
+//送信完了ページ
+Route::post('/contact/thanks', 'ContactController@send')->name('contact.send');
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
